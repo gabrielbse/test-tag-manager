afc0669637313b652e005c8333ed7da719e79980
Gabriel Errico <gabriel@sws.inf.br>
2021-10-13 21:34:42 -0300

    add firebase app credentials

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyBBtxJtnJf-drsU5-q7ANrQIQVhpuYl0Ds",
+	authDomain: "test-tag-manager-f2ee7.firebaseapp.com",
+	projectId: "test-tag-manager-f2ee7",
+	storageBucket: "test-tag-manager-f2ee7.appspot.com",
+	messagingSenderId: "1969848427",
+	appId: "1:1969848427:web:122f0efe4cffe742fdb12e",
+	measurementId: "G-HFT190FPF0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
