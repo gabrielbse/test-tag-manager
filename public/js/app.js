fa6087d88a4e7cdfc18900728e72579eda91e969
Gabriel Errico <gabriel@sws.inf.br>
2021-10-19 22:09:15 -0300

    feat: datalayer

--- public/js/app.js
+++ public/js/app.js
@@ -10,3 +10,22 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+
+function is_checked() {
+    return document.getElementsByClassName("count-checked")[0].checked;
+}
+
+function trigger() {
+	console.log("triggered");
+	return function(e) {
+	  dataLayer.push({
+		'event': 'event.'+e.type,
+		'gtm.element': e.target,
+		'gtm.elementClasses': e.target.className || '',
+		'gtm.elementId': e.target.id || '',
+		'gtm.elementTarget': e.target.target || '',
+		'gtm.elementUrl': e.target.href || e.target.action || '',
+		'gtm.originalEvent': e
+	  });
+	}
+  }
