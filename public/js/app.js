8fe6cc3c4590cf86111645f3eb8fd4c900919dfe
Gabriel Errico <gabriel@sws.inf.br>
2021-10-19 23:07:02 -0300

    fix push dataLayer

--- public/js/app.js
+++ public/js/app.js
@@ -16,16 +16,9 @@ function is_checked() {
 }
 
 function trigger() {
-	console.log("triggered");
-	return function(e) {
-	  dataLayer.push({
-		'event': 'event.'+e.type,
-		'gtm.element': e.target,
-		'gtm.elementClasses': e.target.className || '',
-		'gtm.elementId': e.target.id || '',
-		'gtm.elementTarget': e.target.target || '',
-		'gtm.elementUrl': e.target.href || e.target.action || '',
-		'gtm.originalEvent': e
-	  });
-	}
-  }
+	console.log("trigered");
+	dataLayer.push({
+		'event': 'count-mouseenter',
+		'elementClasses': 'elementClasses'
+	});
+}
